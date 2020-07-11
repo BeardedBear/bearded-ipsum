998f0cec445a83737b692761eeedf71741f80283
BeardedBear <germain.poirrier@gmail.com>
2020-07-11 13:59:31 +0200

    Updates

--- js/about-legacy.bffa4826.js
+++ js/about-legacy.bffa4826.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("Bearded Ipsum")])])}],u=n("2877"),r={},c=Object(u["a"])(r,a,s,!1,null,null,null);t["default"]=c.exports}}]);
-//# sourceMappingURL=about.ffc0b8d6.js.map
+//# sourceMappingURL=about-legacy.bffa4826.js.map
